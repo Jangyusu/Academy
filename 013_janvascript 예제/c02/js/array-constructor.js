5ecc23f34dd5e076c560c3c00a0a3eaabf2e3fbd
yusu <dbtnss@naver.com>
2020-02-26 16:08:58 +0900

    javascript

--- 013_janvascript 예제/c02/js/array-constructor.js
+++ 013_janvascript 예제/c02/js/array-constructor.js
@@ -1,13 +1,37 @@
 // Create and name the variable.
 // Tell the interpreter it is an array.
 // Assign values inside the parentheses.
-var colors = new Array('white', 
-                       'black',
-                       'custom');
-
+var colors = new Array("white", "black", "custom");
+var colors2 = new Array("aa", "bb");
+var el = document.getElementById("colors");
 // Show the first item from the array.
-var el = document.getElementById('colors');
-el.textContent = colors[0];
+
+colors[2] = "beige";
+colors.push("beige2");
+//배열의 끝에 원하는 값을 추가
+
+colors.shift();
+// 첫번째 값을 제거
+
+colors.pop();
+// 마지막 값을 제거
+
+colors = colors.concat(colors2);
+// 두개의 배열을 합쳐줌
+
+var font = ["dotum", "gulim"];
+colors = colors.concat(font);
+// 두개의 배열을 합쳐줌
+
+// colors = colors.join("/");
+// 배열값 사이에 원하는 문자를 삽입
+
+colors = colors.reverse();
+// 배열을 역순으로 재배치
+
+colors = colors.slice(0, 2);
+
+console.log(colors);
 
 /* 
 NOTE: textContent does not work in IE8 or earlier
--- 013_janvascript 예제/c02/js/array-literal.js
+++ 013_janvascript 예제/c02/js/array-literal.js
@@ -1,1 +1,27 @@
-// Create an array and assign it values.var colors; colors = ['white', 'black', 'custom'];// Show the first item from the array.var el = document.getElementById('colors');el.textContent = colors[0];/* NOTE: textContent does not work in IE8 or earlierYou can use innerHTML on line 7, but note the security issues on p228-231el.textContent = colors[0];*/
+// Create an array and assign it values.
+var colors = ["red", "green", "blue"];
+
+// Show the first item from the array.
+var el = document.getElementById("colors");
+el.textContent = colors[0];
+
+var clr = "";
+for (var i in colors) {
+    // clr += "<br><span style=color:";
+    // clr += colors[i] + ">" + colors[i];
+    // clr += "</span>";
+
+    clr += "<br><span>" + colors[i] + "</span>";
+}
+
+el.innerHTML = clr;
+var span = el.getElementsByTagName("span");
+
+for (var i in colors) {
+    span[i].style.color = colors[i];
+}
+/* 
+NOTE: textContent does not work in IE8 or earlier
+You can use innerHTML on line 7, but note the security issues on p228-231
+el.textContent = colors[0];
+*/
